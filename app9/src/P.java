095fd0a0c22f365849155c6fb6ee80356d61b4b3
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 13:12:06 +0530

    Update P.java

--- app9/src/P.java
+++ app9/src/P.java
@@ -4,17 +4,11 @@ class P
 	{
 		System.out.println("main begin");
 		if (false)
-		{
 			System.out.println("if block");
-		}
 		else if (true)
-		{
 			System.out.println("if2");
-		}
 		else if (true)
-		{
 			System.out.println("if3");
-		}
 		System.out.println("main end");
 	}
 }
