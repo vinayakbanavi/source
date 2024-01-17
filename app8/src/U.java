4c7109460019a9ec8f8a7aab401b56a2336dbf91
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 13:01:36 +0530

    Update U.java

--- app8/src/U.java
+++ app8/src/U.java
@@ -4,12 +4,12 @@ class U
 	{
 		System.out.println("main begin");
 		if (true)
-		if (false);
-		System.out.println("if2");
-		else                              //ERROR - ELSE WITHOUT IF
-		System.out.println("else2");
+			if (false);
+			System.out.println("if2");
+			else                              //ERROR - ELSE WITHOUT IF
+				System.out.println("else2");
 		else                                        
-		System.out.println("else1 block");
+			System.out.println("else1 block");
 		System.out.println("main end");
 	}
 }
