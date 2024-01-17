5d37fb0989d0b1974bf2bd6cdd59592c15c3be06
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:33:06 +0530

    Update K.java

--- app7/src/K.java
+++ app7/src/K.java
@@ -2,22 +2,22 @@ class K
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (true)
-        {
-         System.out.println("if1 begin");
-		 if (true)
-		     {
-             System.out.println("if2 begin");
-			 if (true)
-			     {
-				 System.out.println("if3 begin");
-				 System.out.println("if3 end");
-			     }
-		   	 System.out.println("if2 end");
-		     }
-		 System.out.println("if1 end");
-        }
-	System.out.println("main end");
+		System.out.println("main start");
+    		if (true)
+	        {
+	         	System.out.println("if1 begin");
+			if (true)
+			{
+				System.out.println("if2 begin");
+				if (true)
+				{
+					System.out.println("if3 begin");
+					System.out.println("if3 end");
+				}
+				System.out.println("if2 end");
+			}
+			System.out.println("if1 end");
+	        }
+		System.out.println("main end");
 	}
 }
