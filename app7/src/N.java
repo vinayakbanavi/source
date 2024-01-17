424deac968e321ff356e160e8c3be4f0ade388d6
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:35:55 +0530

    Update N.java

--- app7/src/N.java
+++ app7/src/N.java
@@ -2,17 +2,17 @@ class N
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (true)
-        {
-         System.out.println("if1 begin");
-		 if (true)
-		     {
-			   if (true)
-			   System.out.println("if3");
-		     }
-		 System.out.println("if1 end");
-        }
-	System.out.println("main end");
+		System.out.println("main start");
+		if(true)
+		{
+			System.out.println("if1 begin");
+			if(true)
+			{
+				if(true)
+					System.out.println("if3");
+			}
+			System.out.println("if1 end");
+		}
+		System.out.println("main end");
 	}
 }
