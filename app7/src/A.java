526bbe61d5a30ab095df25494d7825affd0fc986
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:24:57 +0530

    Update A.java

--- app7/src/A.java
+++ app7/src/A.java
@@ -5,14 +5,14 @@ class A
 		System.out.println("main begin");
 		if (true)
 		{
-            System.out.println("if1 begin");
-		    if(true)
-			   {
-			      System.out.println("if2 begin");
-			      System.out.println("if2 end");
-			   }
-			System.out.println("if1 end");
+            		System.out.println("if1 begin");
+		    	if(true)
+			{
+				System.out.println("if2 begin");
+				System.out.println("if2 end");
+			}
+				System.out.println("if1 end");
 		}
-        System.out.println("main end");
+        	System.out.println("main end");
 	}
 }
