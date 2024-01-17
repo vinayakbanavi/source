267cdbc65f8f10f07a71b7d9970eb9744e1a224d
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 13:06:24 +0530

    Update I.java

--- app9/src/I.java
+++ app9/src/I.java
@@ -8,13 +8,13 @@ class I
 			System.out.println("if block");
 		}
 		else if (false)
-			{
-				System.out.println("if2");
-			}
-			else
-			{
-				System.out.println("else 2");
-			}
+		{
+			System.out.println("if2");
+		}
+		else
+		{
+			System.out.println("else 2");
+		}
 		System.out.println("main end");
 	}
 }
