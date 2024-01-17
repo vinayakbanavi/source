cc5c05c2431b4cd31276fae7918eb282fda8b923
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:12:59 +0530

    Update V.java

--- app6/src/V.java
+++ app6/src/V.java
@@ -4,18 +4,19 @@ class V
 	{
 		System.out.println("main begin");
 
-    {
-        System.out.println("block1 -stmt1");
-        System.out.println("block1 -stmt2");
-        System.out.println("block1 -stmt3");
-        System.out.println("block1 -stmt4");
-	}
-    {
-        System.out.println("block2 -stmt1");
-        System.out.println("block2 -stmt2");
-        System.out.println("block2 -stmt3");
-        System.out.println("block2 -stmt4");
-	}
-	    System.out.println("main end");
+		{
+		        System.out.println("block1 -stmt1");
+		        System.out.println("block1 -stmt2");
+		        System.out.println("block1 -stmt3");
+		        System.out.println("block1 -stmt4");
+		}
+	   	{
+		        System.out.println("block2 -stmt1");
+		        System.out.println("block2 -stmt2");
+		        System.out.println("block2 -stmt3");
+		        System.out.println("block2 -stmt4");
+		}
+		
+		System.out.println("main end");
 	}
 }
