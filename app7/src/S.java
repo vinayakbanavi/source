e1ac1c1bdfed2a03b78415b20f8241d0ee5b086a
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:39:47 +0530

    Update S.java

--- app7/src/S.java
+++ app7/src/S.java
@@ -2,11 +2,11 @@ class S
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (false);
-	if (true)
-    if (true)
-    System.out.println("if3");
-	System.out.println("main end");
+		System.out.println("main start");
+		if(false);
+		if(true)
+			if(true)
+				System.out.println("if3");
+		System.out.println("main end");
 	}
 }
