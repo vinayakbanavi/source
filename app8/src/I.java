2c0d1a96bc3140f73ada55d1fb2b9bf04b424933
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:52:43 +0530

    Update I.java

--- app8/src/I.java
+++ app8/src/I.java
@@ -6,16 +6,14 @@ class I
 		System.out.println("main begin" + i);
 		if (++i == i++)
 		{
-           System.out.println("if block" + i);
-           i += 20;
+			System.out.println("if block" + i);
+			i += 20;
 		}
-
 		else
 		{    
-		   
-		    System.out.println("else block" + i);
+			System.out.println("else block" + i);
 			i += 30;
 		}
-        System.out.println("main end" + i);
+       		System.out.println("main end" + i);
 	}
 }
