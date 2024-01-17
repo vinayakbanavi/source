08f47e860cf78dc64311dfbf11cfa2fa91d11802
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 13:00:27 +0530

    Update S.java

--- app8/src/S.java
+++ app8/src/S.java
@@ -4,12 +4,12 @@ class S
 	{
 		System.out.println("main begin");
 		if (true)
-		if (false)
-		System.out.println("if2");
+			if (false)
+				System.out.println("if2");
+			else
+				System.out.println("else2");
 		else
-		System.out.println("else2");
-		else
-		System.out.println("else1 block");
+			System.out.println("else1 block");
 		System.out.println("main end");
 	}
 }
