7a3130829b2a6dc983b3d13896b19023266a7243
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:36:35 +0530

    Update O.java

--- app7/src/O.java
+++ app7/src/O.java
@@ -2,15 +2,15 @@ class O
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (true)
-        {
-         System.out.println("if1 begin");
-		 if (true)
-		 if (true)
-		 System.out.println("if3");
-		 System.out.println("if1 end");
-        }
-	System.out.println("main end");
+		System.out.println("main start");
+		if(true)
+		{
+			System.out.println("if1 begin");
+			if(true)
+				if(true)
+					System.out.println("if3");
+			System.out.println("if1 end");
+		}
+		System.out.println("main end");
 	}
 }
