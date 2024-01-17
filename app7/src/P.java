56b4bb2162528e4fb55365015ad278750e6be4f7
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:38:02 +0530

    Update P.java

--- app7/src/P.java
+++ app7/src/P.java
@@ -2,13 +2,13 @@
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (true)
-        {
-		 if (true)
-		 if (true)
-		 System.out.println("if3");
-        }
-	System.out.println("main end");
+		System.out.println("main start");
+		if(true)
+		{
+			if(true)
+				if(true)
+					System.out.println("if3");
+		}
+		System.out.println("main end");
 	}
 }
