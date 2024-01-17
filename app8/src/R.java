e350d912e499334e40d5a1efe108eb244ad1a1d4
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 13:00:08 +0530

    Update R.java

--- app8/src/R.java
+++ app8/src/R.java
@@ -4,12 +4,12 @@
 	{
 		System.out.println("main begin");
 		if (false)
-		if (true)
-		System.out.println("if2");
+			if (true)
+				System.out.println("if2");
+			else
+				System.out.println("else2");
 		else
-		System.out.println("else2");
-		else
-		System.out.println("else1 block");
+			System.out.println("else1 block");
 		System.out.println("main end");
 	}
 }
