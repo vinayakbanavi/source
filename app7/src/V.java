b1d5c9f3bde3e3398651194f7f67bb28b825b502
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:41:22 +0530

    Update V.java

--- app7/src/V.java
+++ app7/src/V.java
@@ -3,14 +3,14 @@ class V
 	public static void main(String[] args) 
 	{
 		System.out.println("main begin");
-		if (true)
-		System.out.println("if1");
-		if (true)
-		System.out.println("if2");
-		if (true)
-		System.out.println("if3");
-		if (true)
-		System.out.println("if4");
+		if(true)
+			System.out.println("if1");
+		if(true)
+			System.out.println("if2");
+		if(true)
+			System.out.println("if3");
+		if(true)
+			System.out.println("if4");
 	}
 }
 /*
