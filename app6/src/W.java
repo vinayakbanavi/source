a6cbf7e5eb9e95192aaa4a8b3c923137e20da061
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:18:21 +0530

    Update W.java

--- app6/src/W.java
+++ app6/src/W.java
@@ -3,20 +3,21 @@ class W
 	public static void main(String[] args) 
 	{
 		System.out.println("main begin");
-
-    {
-		int i = 10;
-        System.out.println("block1 -stmt1");
-        System.out.println("block1 -stmt2");
-        System.out.println("block1 -stmt3");
-        System.out.println("block1 -stmt4" + i);
-	}
-    {
-        System.out.println("block2 -stmt1" + i);
-        System.out.println("block2 -stmt2");
-        System.out.println("block2 -stmt3");
-        System.out.println("block2 -stmt4");
-	}
-	    System.out.println("main end");
+	
+			{
+				int i = 10;
+			        System.out.println("block1 -stmt1");
+			        System.out.println("block1 -stmt2");
+			        System.out.println("block1 -stmt3");
+			        System.out.println("block1 -stmt4" + i);
+			}
+			{
+			        System.out.println("block2 -stmt1" + i);
+			        System.out.println("block2 -stmt2");
+			        System.out.println("block2 -stmt3");
+			        System.out.println("block2 -stmt4");
+			}
+		
+		System.out.println("main end");
 	}
 }
