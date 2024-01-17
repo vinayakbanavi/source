c7d37bc98cd089984e543a26c6cdf3b25ea93698
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:40:42 +0530

    Update U.java

--- app7/src/U.java
+++ app7/src/U.java
@@ -2,11 +2,11 @@ class U
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (false);
-	if (false);
-    if (true)
-    System.out.println("if3");
-	System.out.println("main end");
+		System.out.println("main start");
+		if(false);
+		if(false);
+		if(true)
+			System.out.println("if3");
+		System.out.println("main end");
 	}
 }
