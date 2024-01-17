0c4e49ab0a70d1232328e506def26f7a39c6d823
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:38:39 +0530

    Update Q.java

--- app7/src/Q.java
+++ app7/src/Q.java
@@ -2,11 +2,11 @@ class Q
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (true)
-	if (true)
-    if (true)
-    System.out.println("if3");
-	System.out.println("main end");
+		System.out.println("main start");
+		if(true)
+			if(true)
+				if(true)
+					System.out.println("if3");
+		System.out.println("main end");
 	}
 }
