fcd972cf2616ec482b265e40b93ab15acda2b67e
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:42:11 +0530

    Update W.java

--- app7/src/W.java
+++ app7/src/W.java
@@ -3,14 +3,14 @@ class W
 	public static void main(String[] args) 
 	{
 		System.out.println("main begin");
-		if (true)
-		System.out.println("if1");
-		if (true)
-		System.out.println("if2");
-		if (false)
-		System.out.println("if3");
-		if (true)
-		System.out.println("if4");
+		if(true)
+			System.out.println("if1");
+		if(true)
+			System.out.println("if2");
+		if(false)
+			System.out.println("if3");
+		if(true)
+			System.out.println("if4");
 	}
 }
 /*
