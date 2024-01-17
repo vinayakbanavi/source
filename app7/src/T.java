5388e89399230f9b7a84cb2496dfdf6e684451f4
vinayakbanavi <132206598+vinayakbanavi@users.noreply.github.com>
2024-01-17 12:40:19 +0530

    Update T.java

--- app7/src/T.java
+++ app7/src/T.java
@@ -2,11 +2,11 @@ class T
 {
 	public static void main(String[] args) 
 	{
-	System.out.println("main start");
-    if (false);
-	if (false)
-    if (true)
-    System.out.println("if3");
-	System.out.println("main end");
+		System.out.println("main start");
+		if(false);
+		if(false)
+			if(true)
+				System.out.println("if3");
+		System.out.println("main end");
 	}
 }
